108fa6d8378d1a8acf9d433f1d356d7563b38fd9
Brian Stout <brianjonstout@gmail.com>
2017-05-19 13:04:49 -0400

    Program works, replaced loop counter with ecx, working on overflow discovery.

--- fibonacci.s
+++ fibonacci.s
@@ -1,7 +1,16 @@
 .intel_syntax noprefix
 
+Format:
+	.asciz "%ld\n"
+
 ZeroResult:
 	.asciz "0"
+
+OneResult:
+	.asciz "1"
+
+TwoResult:
+	.asciz "2"
 
 .globl main
 main:
@@ -13,13 +22,50 @@ main:
 	call strtol #dumps command line argument to eax
 
 	cmp eax, 0
-	je Exit
+	je ExitZero
+
+	cmp eax, 1
+	je ExitOne
+
+	cmp eax, 2
+	je ExitTwo
+
+	mov ecx, eax #r15d is counter
+	sub ecx, 2
+	mov rax, 1
+	inc rbx
+	inc rdx
+
+1:
+	add rax, rbx
+	mov rbx, rdx
+	mov rdx, rax
+
+	sub ecx, 1
+	jnz 1b
+
+	push rbp
+	mov rdi, OFFSET Format
+	mov rsi, rax
+	call printf
 
 	ret
 
 
-Exit:
+ExitZero:
     mov rdi, OFFSET ZeroResult
+    call puts
+    mov eax, 1
+    ret
+
+ExitOne:
+    mov rdi, OFFSET OneResult
+    call puts
+    mov eax, 1
+    ret
+
+ExitTwo:
+    mov rdi, OFFSET TwoResult
     call puts
     mov eax, 1
     ret
